b954c8d8651d42de70ac22c5f585f62cca17b514
Stepan Burlakov <burlackoff@gmail.com>
2022-04-26 13:33:15 +0300

    hotfix: Добавление обработчиков кнопок лайк и корзина

--- script.js
+++ script.js
@@ -75,54 +75,61 @@ popupCard.addEventListener('submit', formSubmitImage);
 //end
 
 //Реализация кнопок лайка, удаления и модального окна просмотра картинки
-const popupImage = document.querySelector('#popup_image');
-const closeButtonPopupImage = popupImage.querySelector('.popup__button');
+// const popupImage = document.querySelector('#popup_image');
+// const closeButtonPopupImage = popupImage.querySelector('.popup__button');
 
-const list = document.querySelector('.cards');
-list.addEventListener('click', function (el) {
-  let button = el.target;
-  //Like button
-  if (button.closest('.card__like-button')) {
-    button.closest('.card__like-button').classList.toggle('card__like-button_active');
-  }
-  //delet button
-  else if (button.closest('.card__trash-button')) {
-    button.closest('li').remove();
-  }
-  //Открытие модального окна просмотра картинки
-  else if (button.closest('.card__image')) {
-    openPopup(popupImage);
+// const list = document.querySelector('.cards');
+// list.addEventListener('click', function (el) {
+//   let button = el.target;
+//   //Like button
+//   if (button.closest('.card__like-button')) {
+//     button.closest('.card__like-button').classList.toggle('card__like-button_active');
+//   }
+//   //delet button
+//   else if (button.closest('.card__trash-button')) {
+//     button.closest('li').remove();
+//   }
+//   //Открытие модального окна просмотра картинки
+//   else if (button.closest('.card__image')) {
+//     openPopup(popupImage);
 
-    //Заменям картику
-    const imageClick = popupImage.querySelector('.popup__image');
-    imageClick.src = button.closest('.card__image').src;
+//     //Заменям картику
+//     const imageClick = popupImage.querySelector('.popup__image');
+//     imageClick.src = button.closest('.card__image').src;
 
-    //Заменяем подпись
-    const imageSubtitle = popupImage.querySelector('.popup__subtitle');
-    const card = button.closest('.card__image').closest('.card');
-    subtitle = card.querySelector('.card__title').textContent;
-    imageSubtitle.textContent = subtitle;
-  }
-})
+//     //Заменяем подпись
+//     const imageSubtitle = popupImage.querySelector('.popup__subtitle');
+//     const card = button.closest('.card__image').closest('.card');
+//     subtitle = card.querySelector('.card__title').textContent;
+//     imageSubtitle.textContent = subtitle;
+//   }
+// })
 
-closeButtonPopupImage.addEventListener('click', function () {
-  closePopup(popupImage);
-});
+// closeButtonPopupImage.addEventListener('click', function () {
+//   closePopup(popupImage);
+// });
+
+const listCards = document.querySelector('.cards__list');
+const formPopupCard = popupCard.querySelector('form');
+const templateCard = document.querySelector('#template_card').content;
 
 //Функция создание карточек
 function creatCard(name, link) {
-  const formPopupCard = popupCard.querySelector('form');
-  const listCards = document.querySelector('.cards__list');
-  const templateCard = document.querySelector('#template_card').content;
-  const templateElement = templateCard.querySelector('li').cloneNode(true);
+  const templateElement = templateCard.querySelector('li').cloneNode(true); //клонируем карточку
+  const buttonLike = templateElement.querySelector('.card__like-button');
+  const buttonTrash = templateElement.querySelector('.card__trash-button');
+  const buttonImage = templateElement.querySelector('.card__image');
 
   templateElement.querySelector('.card__title').textContent = name;
   templateElement.querySelector('.card__image').src = link;
   templateElement.querySelector('.card__image').alt = name;
-  listCards.prepend(templateElement);
+  listCards.prepend(templateElement); //Вставка карточки в начало списка
 
-  popupCard.classList.remove('popup_opened');
-  formPopupCard.reset();
+  popupCard.classList.remove('popup_opened'); //Закрытие модалки после создание карточки
+  formPopupCard.reset(); //Обнуление формы модалки создания карточки
+  
+  buttonLike.addEventListener('click', () => buttonLike.classList.toggle('card__like-button_active')); //Добавление обработчика лайков
+  buttonTrash.addEventListener('click', () => buttonTrash.closest('li').remove()); //Добавление обработчика удаление карточки
 };
 
 //Функция загрузки стартовых карточек
@@ -131,3 +138,4 @@ function renderCard(arrayCard) {
 };
 
 renderCard(initialCards);
+
