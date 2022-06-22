f6cde693c76cf14e7a0105494acb71374880f0b2
Stepan Burlakov <burlackoff@gmail.com>
2022-06-22 18:35:39 +0300

    refactor: add func for UI popup

--- script.js
+++ script.js
@@ -29,20 +29,27 @@ const listCards = document.querySelector('.cards__list');
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
-  document.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
-      closePopup(popup)
-    }
-  })
-  popup.addEventListener('click', event => {
-    if (event.target === popup) {
-      closePopup(popup)
-    }
-  })
+  document.addEventListener('keydown', escClose)
+  popup.addEventListener('click', overlayClose)
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', escClose);
+  popup.removeEventListener('click', overlayClose);
+}
+
+function escClose(event) {
+  if (event.key === 'Escape') {
+    closePopup(document.querySelector('.popup_opened'))
+  }
+}
+
+function overlayClose(event) {
+  const popup = document.querySelector('.popup_opened')
+  if (event.target === popup) {
+    closePopup(popup)
+  }
 }
 
 function handleProfileEditFormSubmit(evt) {
