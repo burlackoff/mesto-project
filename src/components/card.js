82c30fd0554ec0d142c869e7e8c8cd6c012002f5
Stepan Burlakov <burlackoff@gmail.com>
2022-06-28 18:50:41 +0300

    fixbugs: change name functions

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,7 @@ import {openPopup} from './modal.js';
 import {imageClick, imageSubtitle, popupImage, templateCard} from './utils.js';
 
 export function createCard(name, link) {
-  const templateElement = cardClone()
+  const templateElement = getCardTemplate()
   const buttonLike = templateElement.querySelector('.card__like-button');
   const buttonTrash = templateElement.querySelector('.card__trash-button');
   const image = templateElement.querySelector('.card__image');
@@ -11,24 +11,23 @@ export function createCard(name, link) {
   image.src = link;
   image.alt = name;
   
-  setEventListner(buttonLike, buttonTrash, templateElement)
-  //Обработчик просмотра картинки
+  setEventListner(buttonLike, buttonTrash, templateElement, image, name, link)
+
+  return templateElement;
+};
+
+function setEventListner(like, trash, card, image, name, link) {
+  like.addEventListener('click', () => like.classList.toggle('card__like-button_active')); //Добавление обработчика лайков
+  trash.addEventListener('click', () => card.remove()); //Добавление обработчика удаление карточки
   image.addEventListener('click', () => {
     openPopup(popupImage); //Открытие модалки
     imageClick.src = link; //Заменяем картинку
     imageClick.alt = name; //Прописываем alt
     imageSubtitle.textContent = name; //Заменяем подпись      
   });
-
-  return templateElement;
-};
-
-function setEventListner(like, trash, card) {
-  like.addEventListener('click', () => like.classList.toggle('card__like-button_active')); //Добавление обработчика лайков
-  trash.addEventListener('click', () => card.remove()); //Добавление обработчика удаление карточки
 }
 
-function cardClone() {
+function getCardTemplate() {
   const element = templateCard.querySelector('li').cloneNode(true);
   return  element;
 }
