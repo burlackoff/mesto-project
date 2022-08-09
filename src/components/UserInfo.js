4d9ac3f356a4022625e10779e5371f3adcaf4f8c
burlackoff <burlackoff@gmail.com>
2022-08-09 19:19:43 +0300

    feat: add class UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,20 @@
 export default class UserInfo {
   constructor(selectors) {
-    this._selectors = selectors;
+    this._nameProfile = document.querySelector(selectors.name);
+    this._aboutProfile = document.querySelector(selectors.about);
+    this._avatarProfile = document.querySelector(selectors.avatar);
   }
 
   getUserinfo() {
-    
+    return {
+      name: this._nameProfile.textContent,
+      about: this._aboutProfile.textContent
+    }
   }
 
-  setUserInfo() {
-
+  setUserInfo(data) {
+    this._nameProfile.textContent = data.name;
+    this._aboutProfile.textContent = data.about;
+    this._avatarProfile.src = data.avatar;
   }
 }
